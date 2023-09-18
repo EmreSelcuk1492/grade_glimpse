fe129b5779185f1631ac9c0e3da3511fbf7e04a7
Emre Selcuk <emrewii@live.com>
2023-09-18 19:05:48 +0900

    Update InstagramAuth.tsx

--- src/components/InstagramAuth.tsx
+++ src/components/InstagramAuth.tsx
@@ -6,7 +6,11 @@ const InstagramAuth: React.FC = () => {
   const handleRedirect = () => {
     // Define the Instagram OAuth URL with your client_id and redirect_uri
     const instagramOAuthUrl =
-      "https://api.instagram.com/oauth/authorize";
+      "https://api.instagram.com/oauth/authorize" +
+      "?client_id=763568429111393" + // Replace with your Instagram Client ID
+      "&https://grade-glimpse-emreselcuk1492.vercel.app/" + // Replace with your Redirect URI
+      "&scope=user_profile,user_media" +
+      "&response_type=code";
 
     // Redirect the user to Instagram's OAuth flow
     window.location.href = instagramOAuthUrl;
